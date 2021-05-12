88312f18bb2d6fd8f40e2b67ea299c1036484094
maria2021-ops <81684569+maria2021-ops@users.noreply.github.com>
2021-05-12 09:21:46 +0400

    Update scriptjava.js

--- public/Hobbies/scriptjava.js
+++ public/Hobbies/scriptjava.js
@@ -147,7 +147,7 @@ function loadExistingData() {
 function displayData(existingData) {
   document.getElementById("existingData").innerHTML = "<ul>";
   for (var i = 0; i < existingData.length; i++) {
-    currentBook = existingData[i];
-    document.getElementById("existingData").innerHTML += "<li><i>" + currentBook.fullname + "</li> : <b>" + currentBook.title + "</b></li>";
+    currentPhoto = existingData[i];
+    document.getElementById("existingData").innerHTML += "<li><i>" + currentPhoto.fullname + "</li> : <b>" + currentPhoto.title + "</b></li>";
   }
 }
