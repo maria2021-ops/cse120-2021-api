ccc3bc2b060ac0315cdc357431b4a63d4c2614d8
maria2021-ops <81684569+maria2021-ops@users.noreply.github.com>
2021-05-15 12:51:53 +0400

    Add files via upload

---
+++ cse120-2021-api-main 2/server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bodyParser  = require('body-parser');
+const port = process.env.PORT || 3001;
+const app = express();
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId; 
+
+const uri =
+  "mongodb+srv://maria-admin:Hayastan-2021@cluster0.ikofn.mongodb.net/cse120-2021-db";
+
+const client = new MongoClient(uri);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header('Cache-Control',
+               'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods',
+                  'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Max-Age', '30000');
+    res.setHeader('Access-Control-Allow-Headers',
+                  'Content-Type, Authorization, X-Requested-With');
+    next();
+});  
+
+app.get('/', function (req, res) {
+  res.render('index', {});
+})
+
+app.post('/', function (req, res) {
+  res.send({"message":"Sent"});
+})
+
+app.get('/data', function (req, res) {
+  client.connect()
+  .then(client => {
+    client.db('cse120-2021-db').collection('books').find().toArray()
+      .then(results => {
+        console.log(results)
+        res.send({"data":results});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
+app.post('/data', function (req, res) {
+  client.connect()
+  .then(client => {
+    client.db('cse120-2021-db').collection('books').insertOne(req.body)
+      .then(result => {
+        console.log(result)
+        res.send({"message":"Added"});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
+app.post('/data/delete', function (req, res) {
+  client.connect()
+  .then(client => {
+    let id = req.body.id;
+    const query = { "_id": ObjectId(id)};
+    client.db('cse120-2021-db').collection('books').deleteOne(query)
+      .then(result => {
+        console.log(result.deletedCount)
+        res.send({"deleted":result.deletedCount});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
+app.post('/data/update', function (req, res) {
+  //Todo: Please replace this with edit update code
+  client.connect()
+  .then(client => {
+    let id = req.body.id;
+    let newValue = req.body.value;
+    const query = { "_id": ObjectId(id)};
+    client.db('cse120-2021-db').collection('books').update(query,{$set: req.body})
+      .then(result => {
+        console.log(result)
+        res.send({"message":"Updated"});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3001!')
+})
+
+app.get('*', function(req, res) {
+        res.status(404).send('Bad/Default Route.');
+});
