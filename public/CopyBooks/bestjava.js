f2b2966fcf28228511d3184f24faab2096dbf601
maria2021-ops <81684569+maria2021-ops@users.noreply.github.com>
2021-05-15 12:57:38 +0400

    Add files via upload

---
+++ public/CopyBooks/bestjava.js
@@ -0,0 +1,207 @@
+var requiredFields = [ "anun" , "vernagir", "grox", "guyn"
+]
+
+
+var BestBook = {
+  "project": "Books",
+  "owner": "Maria Yeritsyan",
+  "anun": "",
+  "vernagir": "",
+  "grox": "",
+  "guyn": "",
+  "coverurish": "",
+  "EjiQanak": "",
+  "Gin": "",
+  "Pox": "",
+  "bnakan": "",
+  "Anpetq": "",
+  "Chapser": "",
+  "Tpagrox": "",
+  "Or": "",
+  "IskakanOr": "",
+  "Tesak": "",
+  "Tariq": "",
+  "language": "",
+  "grelvalue": "",
+}
+
+
+
+function HandleFullNameChange() {
+  BestBook.anun = document.getElementById("anun").value;
+}
+
+function HandleVernagirChange() {
+  BestBook.vernagir = document.getElementById("vernagir").value;
+}
+
+function HandleGroxChange() {
+  BestBook.grox = document.getElementById("grox").value;
+}
+
+function HandleGuynChange() {
+  BestBook.guyn = document.getElementById("guyn").value;
+}
+
+function HandleVorakChange(e) {
+  BestBook.language = e.target.value;
+  if (BestBook.language != "other") {
+    BestBook.coverurish = "";
+    document.getElementById("coverurish").style.display = "none";
+  }
+  else {
+    document.getElementById("coverurish").style.display = "block";
+  }
+}
+
+function HandleLavVorakChange() {
+  if (BestBook.language == "other") {
+    BestBook.coverurish = document.getElementById("coverurish").value;
+    document.getElementById("coverurish").style.display = "block";
+  }
+}
+
+function HandleEjiQanakChange() {
+  BestBook.EjiQanak = document.getElementById("EjiQanak").value;
+}
+
+function HandleGinChange() {
+  BestBook.Gin = document.getElementById("Gin").value;
+}
+
+function HandlePoxChange() {
+  BestBook.Pox = document.getElementById("Pox").value;
+}
+
+
+function Handlelanguagechange(e) {
+  BestBook.language = e.target.value;
+  if (BestBook.language != "other") {
+    BestBook.grelvalue = "";
+    document.getElementById("grel").style.display = "none";
+  }
+  else {
+    document.getElementById("grel").style.display = "block";
+  }
+}
+
+function Handlegrelchange() {
+  if (BestBook.language == "other") {
+    BestBook.grelvalue = document.getElementById("grel").value;
+    document.getElementById("grel").style.display = "block";
+  }
+}
+
+function HandleIskakanLanguageChange(e) {
+  BestBook.language = e.target.value;
+  if (BestBook.language != "other") {
+    BestBook.language = "";
+    document.getElementById("bnakan").style.display = "none";
+  }
+  else {
+    document.getElementById("bnakan").style.display = "block";
+  }
+}
+
+function HandleIskakanLezuChange() {
+  if (BestBook.language == "other") {
+    BestBook.language = document.getElementById("bnakan").value;
+    document.getElementById("bnakan").style.display = "block";
+  }
+}
+
+function HandleAnpetqChange() {
+  BestBook.Anpetq = document.getElementById("Anpetq").value;
+}
+
+function HandleChapserChange() {
+  BestBook.Chapser = document.getElementById("Chapser").value;
+}
+
+function HandleTpagroxChange() {
+  BestBook.Tpagrox = document.getElementById("Tpagrox").value;
+}
+
+function HandleOrChange() {
+  BestBook.Or = document.getElementById("Or").value;
+}
+
+function HandleIskakanOrChange() {
+  BestBook.IskakanOr = document.getElementById("IskakanOr").value;
+}
+
+function HandleTesakChange() {
+  BestBook.Tesak = document.getElementById("Tesak").value;
+}
+
+function HandleTariqChange() {
+  BestBook.Tariq = document.getElementById("Tariq").value;
+}
+
+function validateFormData() {
+  var isFormValid = true;
+  var keys = Object.keys(BestBook);
+  keys.forEach(key => {
+      if (requiredFields.indexOf(key) > -1 && BestBook[key] == "") { console.log(key, " is a required field, please add a value") 
+      if(document.getElementById(key)) {
+        document.getElementById(key).style.backgroundColor = "red"; 
+        isFormValid = false;
+      }
+    }   
+  })
+  return isFormValid;
+}
+
+function ShowTheData(e) {
+  if(validateFormData() == false) {
+    return;
+  } else {
+console.log(BestBook);
+  }
+}
+
+  $.ajax({
+    type: 'POST',
+    url: "https://cse120-2021-api-maria.herokuapp.com/data",
+    data: BestBook,
+    cache: false,
+    dataType: 'json',
+    success: function (data) {
+      console.log("success");
+    
+    },
+    error: function (xhr) {
+      console.error("Error in post", xhr);
+    },
+    complete: function () {
+      console.log("Complete");
+      
+
+    }
+  });
+
+
+function complete () {
+  console.log("Complete");
+ 
+}
+
+
+function loadExistingData() {
+  var existingData = [];
+  $.ajax({
+    type: "GET",
+    url: "https://cse120-2021-api-maria.herokuapp.com/data",
+    data: BestBook,
+    cache: false,
+    dataType: "json",
+    success: function (data) {
+      console.log("success", data);
+      existingData = data;
+      displayData(existingData.data);
+    },
+    error: function (data) {
+      console.log("Error")
+    }
+  });
+}
---
+++ public/Hobbies/scriptjava.js
@@ -0,0 +1,182 @@
+var requiredFields = [ "AnunAzganun" , "SiracAparat", "Nkarahanum", "Haytni"
+]
+
+
+var PhotoHobby = {
+  "project": "Photo",
+  "owner": "Maria Yeritsyan",
+  "AnunAzganun": "",
+  "InternetHxum": "",
+  "SiracAparat": "",
+  "Lusankarich": "",
+  "Nkarahanum": "",
+  "UrishNkar": "",
+  "Nkar": "",
+  "Oracuyc": "",
+  "Haytni": "",
+  "Nastroyka": "",
+  "AmenOr": "",
+  "VochAmenOr": "",
+  "Arajin": "",
+  "Arxiv": "",
+  "UrishArxiv": "",
+
+}
+
+
+function HandleAnunAzganunChange () {
+  PhotoHobby.AnunAzganun=document.getElementById("AnunAzganun").value;
+}
+
+function HandleInternetHxumChange () {
+  PhotoHobby.InternetHxum=document.getElementById("InternetHxum").value;
+}
+
+function HandleSiracAparatChange () {
+  PhotoHobby.SiracAparat=document.getElementById("SiracAparat").value;
+}
+
+function HandleLusankarichChange () {
+  PhotoHobby.Lusankarich=document.getElementById("Lusankarich").value;
+}
+
+function HandleNkarahanumChange () {
+  PhotoHobby.Nkarahanum=document.getElementById("Nkarahanum").value;
+}
+
+function HandleOracuycChange () {
+  PhotoHobby.Oracuyc=document.getElementById("Oracuyc").value;
+}
+
+function HandleNkarChange (e) {
+ PhotoHobby.Nkar=e.target.value;
+ if (PhotoHobby.Nkar!="other") {
+   PhotoHobby.UrishNkar="";
+   document.getElementById("UrishNkar").style.display="none";
+ }
+ else{
+   document.getElementById("UrishNkar").style.display="block";
+ }
+}
+
+function HandleUrishNkarChange() {
+  if (PhotoHobby.Nkar == "other") {
+    PhotoHobby.UrishNkar = document.getElementById("UrishNkar").value;
+    document.getElementById("UrishNkar").style.display="block";
+  }
+}
+
+function HandleHaytniChange () {
+  PhotoHobby.Haytni=document.getElementById("Haytni").value;
+}
+
+function HandleNastroykaChange () {
+  PhotoHobby.Nastroyaka=document.getElementById("Nastroyka").value;
+}
+
+function HandleAmenOrChange () {
+  PhotoHobby.AmenOr=document.getElementById("AmenOr").value;
+}
+
+function HandleVochAmenOrChange () {
+  PhotoHobby.VochAmenOr=document.getElementById("VochAmenOr").value;
+}
+
+function HandleArajinChange () {
+  PhotoHobby.Arajin=document.getElementById("Arajin").value;
+}
+
+function HandleArxivChange (e) {
+ PhotoHobby.Arxiv=e.target.value;
+ if (PhotoHobby.Arxiv!="other") {
+   PhotoHobby.UrishArxiv="";
+   document.getElementById("UrishArxiv").style.display="none";
+ }
+ else{
+   document.getElementById("UrishArxiv").style.display="block";
+ }
+}
+
+function HandleUrishArxivChange() {
+  if (PhotoHobby.Arxiv == "other") {
+    PhotoHobby.UrishArxiv = document.getElementById("UrishArxiv").value;
+    document.getElementById("UrishArxiv").style.display="block";
+  }
+}
+
+function validateFormData() {
+  var isFormValid = true;
+  var keys = Object.keys(PhotoHobby);
+  keys.forEach(key => {
+      if (requiredFields.indexOf(key) > -1 && PhotoHobby[key] == "") { console.log(key, " is a required field, please add a value") 
+      if(document.getElementById(key)) {
+        document.getElementById(key).style.backgroundColor = "red"; 
+        isFormValid = false;
+      }
+    }   
+  })
+  return isFormValid;
+}
+
+function ShowTheData(e) {
+  if(validateFormData() == false) {
+    return;
+  } else {
+console.log(PhotoHobby);
+  }
+}
+
+
+function ShowTheData(e) {
+  e.preventDefault();
+  console.log(PhotoHobby);
+  $.ajax({
+    type: 'POST',
+    url: "https://cse120-2021-api-maria.herokuapp.com/data",
+    data: PhotoHobby,
+    cache: false,
+    dataType : 'json',
+    success: function (data) {
+      console.log("success");
+    },
+    error: function (xhr) {
+      console.error("Error in post", xhr);
+    },
+    complete: function () {
+      console.log("Complete");  
+      document.location="https://cse120-2021-api-maria.herokuapp.com/";
+    }
+  });
+}
+
+function complete () {
+	console.log("Complete");
+
+}
+
+function loadExistingData() {
+	var existingData = [];
+  $.ajax({
+    type : "GET",
+    url : "https://cse120-2021-api-maria.herokuapp.com/data",
+    data: PhotoHobby,
+    dataType : "json",
+    success : function(data) {
+      console.log("success", data);
+      existingData = data;
+      displayData(existingData.data);
+      
+    },
+    error : function(data) {
+        console.error("Error: in post");
+    }
+  });
+}
+
+function displayData(existingData) {
+  document.getElementById("existingData").innerHTML = "<ul>";
+  for (var i = 0; i < existingData.length; i++) {
+    currentBook = existingData[i];
+    document.getElementById("existingData").innerHTML += "<li><i>" + currentBook.fullname + "</li> : <b>" + currentBook.title + "</b></li>";
+  }
+}
